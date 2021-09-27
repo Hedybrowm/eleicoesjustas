8524074608d7393a8458d038ed4de4e8a6072533
Barros7 <josebarros412@gmail.com>
2021-09-27 20:48:18 +0100

    Base de dados de eleições seguras criada, não avaliada.

--- Database/eleicoes_justasDB.sql
+++ Database/eleicoes_justasDB.sql
@@ -1,9 +1,9 @@
 CREATE TABLE assembleias(
-	codigo_assembleia int not null auto_increment,
+	codigo_assembleia int PRIMARY KEY,
     numero int,
     nome varchar(50),
-    codigo_localidade int,
-    PRIMARY KEY (codigo_assembleia)
+    foto varchar(32),
+    codigo_localidade int
 );
 
 CREATE TABLE votos_presidentes(
@@ -18,12 +18,12 @@ CREATE TABLE votos_presidentes(
 );
 
 CREATE TABLE mesas_eleitorais(
-    codigo_mesa int not null auto_increment,
+    codigo_mesa int PRIMARY KEY,
     numero int,
     nome varchar(50),
     codigo_assembleia int,
     acta varchar(50),
-    PRIMARY KEY (codigo_mesa),
+    foto varchar(32),
     FOREIGN KEY (codigo_assembleia) REFERENCES assembleias(codigo_assembleia)
 );
 
@@ -39,24 +39,24 @@ CREATE TABLE votos_partidos(
 );
 
 CREATE TABLE delegados(
-	codigo_delegado int not null auto_increment,
+	codigo_delegado int PRIMARY KEY,
     nome_completo varchar(50),
-    nome_utilizador varchar(50),
+    nome_utilizador varchar(20),
     palavra_passe varchar(50),
     tipo char(1),
     BI varchar(15),
     copia_acta varchar(50),
     telemovel varchar(13),
     email varchar(50),
+    foto varchar(32),
     codigo_mesa int,
-    codigo_partido int,
-    PRIMARY KEY (codigo_delegado),
+    codigo_partido int
     FOREIGN KEY (codigo_mesa) REFERENCES mesas_eleitorais(codigo_mesa),
     FOREIGN KEY (codigo_partido) REFERENCES partidos(codigo_partido)
 );
 
 CREATE TABLE presidentes_candidatos(
-    codigo_PR_candidato int not null auto_increment,
+    codigo_PR_candidato int PRIMARY KEY,
     numero int,
     nome varchar(50),
     codigo_partido int,
@@ -65,11 +65,10 @@ CREATE TABLE presidentes_candidatos(
 );
 
 CREATE TABLE partidos(
-    codigo_partido int not null auto_increment,
+    codigo_partido int PRIMARY KEY,
     numero int,
     nome varchar(50),
-    bandeira varchar(50),
-    codigo_PR_candidato int,
-    PRIMARY KEY (codigo_partido),
+    bandeira varchar(32),
+    codigo_PR_candidato int
     FOREIGN KEY (codigo_PR_candidato) REFERENCES presidentes_candidatos(codigo_PR_candidato)
 );
